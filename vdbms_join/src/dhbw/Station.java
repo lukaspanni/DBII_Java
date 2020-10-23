7ccd18f805c0972851de3e12bc7e43b429e71ec5
Lukas Panni <lukas.panni@outlook.de>
2020-10-23 16:13:52 +0200

    Join: NestedLoop + transferTwo

--- vdbms_join/src/dhbw/Station.java
+++ vdbms_join/src/dhbw/Station.java
@@ -1,74 +1,107 @@
 
 package dhbw;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class Station
-{
-	public DataTable mData = new DataTable();
-	private int mValuesTransferred = 0;
-
-	
-	public void resetDataTable() {
-		mData = new DataTable();
-	}
-	public void resetValuesTransferred() {
-		mValuesTransferred = 0;
-	}
-	
-	public int getValuesTransferred() {
-		return mValuesTransferred;
-	}
+public class Station {
+    public DataTable mData = new DataTable();
+    private int mValuesTransferred = 0;
 
 
-	// ************************************************************************
-	// Merge-Join
+    public void resetDataTable() {
+        mData = new DataTable();
+    }
 
-	public void mergeJoin(DataTable result, Iterator<Data> jIt1, Iterator<Data> jIt2) {
-	}
-	
+    public void resetValuesTransferred() {
+        mValuesTransferred = 0;
+    }
 
-	// ************************************************************************
-	// Join with Nested Loops
-	
-	// StationResult
-	public void computeJoinNestedLoops(Station stR, Station stS) {
-	}
-	
+    public int getValuesTransferred() {
+        return mValuesTransferred;
+    }
 
-	// ************************************************************************
-	// Join transferring two relations
 
-	// StationResult
-	public void computeJoinTransferTwo(Station stR, Station stS) {
-	}
-	
+    // ************************************************************************
+    // Merge-Join
 
-	// ************************************************************************
-	// Join transferring one relation
+    public void mergeJoin(DataTable result, Iterator<Data> jIt1, Iterator<Data> jIt2) {
+    }
 
-	// StationResult
-	public void computeJoinTransferOne(Station stR, Station stS) {
-	}
-	
-	
-	// ************************************************************************
-	// Join filtering one relation
+    // ************************************************************************
+    // Merge-Join
+    private void hashJoin(DataTable result, Iterator<Data> iter1, Iterator<Data> iter2) {
+        Map<Integer, List<Data>> joinMap = new HashMap<>();
+        while (iter1.hasNext()) {
+            Data element = iter1.next();
+            if (!joinMap.containsKey(element.C)) {
+                joinMap.put(element.C, new ArrayList<>());
+            }
+            joinMap.get(element.C).add(element);
+        }
+        while (iter2.hasNext()) {
+            Data element = iter2.next();
+            if (!joinMap.containsKey(element.C)) continue;
+            List<Data> list = joinMap.get(element.C);
+            list.forEach(data -> result.addData(new DataResult(data, element)));
+        }
+    }
 
-	// StationResult
-	public void computeJoinFilterOne(Station stR, Station stS) {
-	}
-	
-	
-	// ************************************************************************
-	// Join using hash
 
-	// StationResult
-	public void computeJoinHash(Station stR, Station stS) {
-	}
+    private List<Data> getByKey(int key) {
+        return mData.data().stream().filter(data -> data.C == key).collect(Collectors.toList());
+    }
+
+    // ************************************************************************
+    // Join with Nested Loops
+
+    // StationResult
+    public void computeJoinNestedLoops(Station stR, Station stS) {
+        for (Iterator<Data> it = stR.mData.getIterator(); it.hasNext(); ) {
+            Data r = it.next();
+            //Transfer r from stR to stS
+            mValuesTransferred += 3;
+            List<Data> sData = stS.getByKey(r.C);
+            //Transfer sData from stS to stR
+            mValuesTransferred += sData.size() * 3;
+            sData.forEach(s -> mData.addData(new DataResult(r, s)));
+        }
+    }
+
+
+    // ************************************************************************
+    // Join transferring two relations
+
+    // StationResult
+    public void computeJoinTransferTwo(Station stR, Station stS) {
+        Iterator<Data> rIter = stR.mData.getIterator();
+        Iterator<Data> sIter = stS.mData.getIterator();
+        hashJoin(mData, rIter, sIter);
+        mValuesTransferred += stR.mData.size() * 3 + stS.mData.size() * 3;
+    }
+
+
+    // ************************************************************************
+    // Join transferring one relation
+
+    // StationResult
+    public void computeJoinTransferOne(Station stR, Station stS) {
+    }
+
+
+    // ************************************************************************
+    // Join filtering one relation
+
+    // StationResult
+    public void computeJoinFilterOne(Station stR, Station stS) {
+    }
+
+
+    // ************************************************************************
+    // Join using hash
+
+    // StationResult
+    public void computeJoinHash(Station stR, Station stS) {
+    }
 }
 
