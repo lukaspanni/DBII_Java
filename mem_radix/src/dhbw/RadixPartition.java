f4deb5036a5a160c8ec7a7f17802ac1de344e84f
Lukas Panni <lukas.panni@outlook.de>
2020-11-02 10:54:18 +0100

    Radix-Partition Implementation

--- mem_radix/src/dhbw/RadixPartition.java
+++ mem_radix/src/dhbw/RadixPartition.java
@@ -1,66 +1,121 @@
-
 package dhbw;
 
 
-public class RadixPartition
-{
-	final static int N_MASK_BITS = 1;
-	final static int N_WORKER = (1 << N_MASK_BITS);
-	final static int N_DATA = 6*N_WORKER;	// must be multiple of N_WORKER
-	
-	final static int BITS_VALUE = 6;  // number of bits for our test data
-	final static int MAX_VALUE = (1 << BITS_VALUE);
-	
-	int[] mInputData = new int[N_DATA];
-	int[][] mOutputData = new int[N_WORKER][N_DATA];
-	int[] mOutputDataSize = new int[N_WORKER];
-		
-	Worker[] mWorkers = new Worker[N_WORKER];
+import java.util.concurrent.CyclicBarrier;
 
-			
-	class Worker extends Thread
-	{
-		private int mIndex;
-		private int[] mHistogram = new int[N_WORKER];
-		private int[] mWriteIndex = new int[N_WORKER];
-		
-		Worker(int index) {
-			mIndex = index;
-		}
+public class RadixPartition {
+    final static int N_MASK_BITS = 1;
+    final static int N_WORKER = (1 << N_MASK_BITS); // 2^n
+    final static int N_DATA = 6 * N_WORKER;    // must be multiple of N_WORKER
 
-		@Override
-		public void run() {
-		}
-	}
+    final static int BITS_VALUE = 6;  // number of bits for our test data
+    final static int MAX_VALUE = (1 << BITS_VALUE);
+
+    int[] mInputData = new int[N_DATA];
+    int[][] mOutputData = new int[N_WORKER][N_DATA];
+    int[] mOutputDataSize = new int[N_WORKER];
+
+    Worker[] mWorkers = new Worker[N_WORKER];
+
+    CyclicBarrier mBarrier = new CyclicBarrier(N_WORKER, new BarrierAction());
+
+    class Worker extends Thread {
+        private int mIndex;
+        private int[] mHistogram = new int[N_WORKER];
+        private int[] mWriteIndex = new int[N_WORKER];
+
+        Worker(int index) {
+            mIndex = index;
+        }
+
+        @Override
+        public void run() {
+            //Build Histogramm
+            int size = N_DATA / N_WORKER;
+            int start = mIndex * size;
+            int end = start + size;
+            for (int i = start; i < end; i++) {
+                int idx = getBucket(mInputData[i]);
+                mHistogram[idx]++;
+            }
+            try {
+                //Sync
+                mBarrier.await();
+                //Copy
+                for (int i = start; i < end; i++) {
+                    int idx = getBucket(mInputData[i]);
+                    mOutputData[idx][mWriteIndex[idx]++] = mInputData[i];
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private int getBucket(int inputValue) {
+            // Return n highest bits
+            return (inputValue >> (BITS_VALUE - N_MASK_BITS)) & (1 << N_MASK_BITS) - 1;
+        }
+    }
 
 
-	public static void main(String[] args) {
-		RadixPartition rp = new RadixPartition();
-		rp.partition();
-		System.out.println("done.");
-	}
+    public static void main(String[] args) {
+        RadixPartition rp = new RadixPartition();
+        rp.partition();
+        System.out.println("done.");
+    }
 
-	private void partition() {
-		createInputData();
-		dumpArray("input", mInputData, mInputData.length);
+    private void partition() {
+        createInputData();
+        dumpArray("input", mInputData, mInputData.length);
 
-		for (int i=0; i<N_WORKER; i++)
-			dumpArray("output " + i, mOutputData[i], mOutputDataSize[i]);
-	}
+        for (int i = 0; i < N_WORKER; i++) {
+            mWorkers[i] = new Worker(i);
+            mWorkers[i].start();
+        }
 
-	private void createInputData() {
-		for (int i=0; i<N_DATA; i++)
-			mInputData[i] = randVal();
-	}
+        for (Worker w : mWorkers) {
+            try {
+                w.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-	private int randVal() {
-		return (int)Math.floor(MAX_VALUE * Math.random());
-	}
+        for (int i = 0; i < N_WORKER; i++)
+            dumpArray("output " + i, mOutputData[i], mOutputDataSize[i]);
+    }
 
-	private void dumpArray(String name, int[] array, int size) {
-		System.out.print(name + ": ");
-		for (int i=0; i<size; i++)
-			System.out.print(array[i] + " ");
-		System.out.println();
-	}
+    private void createInputData() {
+        for (int i = 0; i < N_DATA; i++)
+            mInputData[i] = randVal();
+    }
+
+    private int randVal() {
+        return (int) Math.floor(MAX_VALUE * Math.random());
+    }
+
+    private void dumpArray(String name, int[] array, int size) {
+        System.out.print(name + ": ");
+        for (int i = 0; i < size; i++)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+
+    private class BarrierAction implements Runnable {
+        @Override
+        public void run() {
+            //get write indices
+            for(int i = 0; i < N_WORKER; i++){
+                mWorkers[0].mWriteIndex[i] = 0;
+            }
+            for(int i = 1; i < N_WORKER; i++){
+                for(int j = 0; j < N_WORKER; j++){
+                    mWorkers[i].mWriteIndex[j] = mWorkers[i-1].mWriteIndex[j] + mWorkers[i-1].mHistogram[j];
+                }
+            }
+            for(int i = 0; i < N_WORKER; i++){
+                mOutputDataSize[i] = mWorkers[N_WORKER-1].mWriteIndex[i] + mWorkers[N_WORKER-1].mHistogram[i];
+            }
+        }
+    }
 }
